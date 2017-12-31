1bdb03011fe233ea5874008ec8e6b4465f457108
Arthur Poiret <dropsnorz@gmail.com>
2017-12-31 10:43:02 +0000

    Added local jvst repository

--- src/main/java/com/dropsnorz/owlplug/services/PluginExplorer.java
+++ src/main/java/com/dropsnorz/owlplug/services/PluginExplorer.java
@@ -3,43 +3,63 @@ package com.dropsnorz.owlplug.services;
 import java.io.File;
 import java.util.List;
 
+import org.boris.jvst.AEffect;
+import org.boris.jvst.VST;
+import org.boris.jvst.VSTException;
+
 import com.dropsnorz.owlplug.model.OSType;
 import com.dropsnorz.owlplug.model.PluginType;
 import com.dropsnorz.owlplug.services.plugins.NativePluginCollector;
 import com.dropsnorz.owlplug.services.plugins.NativePluginCollectorFactory;
 
 public class PluginExplorer {
-	
+
 	protected String VST2Path;
 	protected String Platform;
-	
+
 	public PluginExplorer(String platform, String VST2Path){
-		
+
 		this.VST2Path = VST2Path;
-		
+
 	}
-	
+
 	public void explore(){
-		
+
 		PluginType[] types = {PluginType.VST2};
 		NativePluginCollector collector = NativePluginCollectorFactory.getPluginFinder(OSType.WIN, types);
 		List<File> files = collector.collect(VST2Path);
-		
+
 		System.out.println("-- WIN VST2 - PLUGINS --");
 		for(File file: files){
-			System.out.println(file.getAbsolutePath());
+
+			System.out.println("LOADING: " + file.getAbsolutePath());
+
+			AEffect a;
+			try {
+				a = VST.load(file.getAbsolutePath());
+				
+				System.out.println(a.getEffectName());
+				System.out.println(a.getProgramName());
+				System.out.println(a.getVersion());
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
 		}
-		
+
 		collector = NativePluginCollectorFactory.getPluginFinder(OSType.MAC, types);
 		files = collector.collect(VST2Path);
-		
+
 		System.out.println("-- OSX VST2 - PLUGINS --");
 		for(File file: files){
 			System.out.println(file.getAbsolutePath());
 		}
-		
-		
+
+
 	}
-	
+
 
 }
