3cabdaacce0eae0fb2f508353be44eee0a529211
Arthur Poiret <dropsnorz@gmail.com>
2018-01-04 17:13:44 +0000

    Update directory view style

--- src/main/java/com/dropsnorz/owlplug/controllers/DirectoryInfoController.java
+++ src/main/java/com/dropsnorz/owlplug/controllers/DirectoryInfoController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 
 import com.dropsnorz.owlplug.model.Plugin;
 import com.dropsnorz.owlplug.model.PluginDirectory;
+import com.jfoenix.controls.JFXListCell;
+import com.jfoenix.controls.JFXListView;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 @Controller
 public class DirectoryInfoController {
@@ -16,11 +21,29 @@ public class DirectoryInfoController {
 	Label directoryPathLabel;
 	
 	@FXML
-	ListView<Plugin> pluginDirectoryListView;
+	JFXListView<Plugin> pluginDirectoryListView;
+	
+	private Image brickImage  = new Image(getClass().getResourceAsStream("/icons/soundwave-blue-16.png"));;
+
 	
 	@FXML
 	public void initialize() { 
 		
+		pluginDirectoryListView.setCellFactory(param -> new JFXListCell<Plugin>() {
+            private ImageView imageView = new ImageView();
+            @Override
+            public void updateItem(Plugin plug, boolean empty) {
+                super.updateItem(plug, empty);
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    imageView.setImage(brickImage);
+                    setText(plug.getName());
+                    setGraphic(imageView);
+                }
+            }
+        });
 
 	}
 	
@@ -28,7 +51,6 @@ public class DirectoryInfoController {
 	public void setPluginDirectory(PluginDirectory pluginDirectory){
 		
 		directoryPathLabel.setText(pluginDirectory.getPath());
-		
 		pluginDirectoryListView.getItems().setAll(pluginDirectory.getPluginList());
 	}
 
