b382ed46fdfb9d2a342ab4204c259c7c1e2e2bfd
Arthur POIRET <dropsnorz@gmail.com>
2018-07-20 22:17:08 +0200

    Update plugin info view, Replace ProgressIndicator with JFXSpinner

--- src/main/java/com/dropsnorz/owlplug/auth/controllers/AccountController.java
+++ src/main/java/com/dropsnorz/owlplug/auth/controllers/AccountController.java
@@ -10,12 +10,12 @@ import com.dropsnorz.owlplug.core.components.LazyViewRegistry;
 import com.dropsnorz.owlplug.core.controllers.MainController;
 import com.dropsnorz.owlplug.core.controllers.dialogs.AbstractDialog;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXSpinner;
 
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.HBox;
 
 @Controller
@@ -37,7 +37,7 @@ public class AccountController extends AbstractDialog {
 	@FXML
 	private JFXButton googleButton;
 	@FXML
-	private ProgressIndicator authProgressIndicator;
+	private JFXSpinner authProgressIndicator;
 	@FXML
 	private Label messageLabel;
 	@FXML
