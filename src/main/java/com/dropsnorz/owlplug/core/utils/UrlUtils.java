d159fc6fe899985c87e1535c8f0145962e31484a
Arthur POIRET <dropsnorz@gmail.com>
2018-11-05 14:15:23 +0100

    Clear useless maven dependencies

--- src/main/java/com/dropsnorz/owlplug/core/utils/UrlUtils.java
+++ src/main/java/com/dropsnorz/owlplug/core/utils/UrlUtils.java
@@ -1,9 +1,12 @@
 package com.dropsnorz.owlplug.core.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class UrlUtils {
 	
 	/**
-	 * Replaces spaces with url char %20
+	 * Replaces spaces with url char %20.
 	 * @param url - The url
 	 * @return url without space chars
 	 */
@@ -13,6 +16,19 @@ public class UrlUtils {
 			return null;
 		}
 		return url.replace(" ", "%20");
+	}
+	
+	/**
+	 * Translates a string into application/x-www-form-urlencodedformat using UTF-8 encoding.
+	 * @param query - the query parameter to encode
+	 * @return an encoded query parameter
+	 */
+	public static String encodeQuery(String query) {
+		try {
+			return URLEncoder.encode(query, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return null;
+		}
 	}
 
 }
