ff1d6b006a74f1b2c74fe08dbcb1a334bb2ad789
Arthur POIRET <dropsnorz@gmail.com>
2019-04-04 23:24:55 +0200

    add license file to msi installer

--- installer/package-msi.cmd
+++ installer/package-msi.cmd
@@ -1,11 +1,17 @@
 @echo off
 
 set owlplug-version=%1
+echo ** Preparing package **
+
+echo ** Exporting LICENSE File **
+
+copy "..\LICENSE" "..\owlplug-client\target\LICENSE"
 
 echo ** Generating OwlPlug MSI Install package **
 
 javapackager -deploy -native msi -appclass org.springframework.boot.loader.JarLauncher ^
--srcdir ../target/ -srcfiles owlplug-%owlplug-version%.jar -outdir ./output -outfile owlplug-app ^
--name OwlPlug -BappVersion=%owlplug-version% -Bicon=resources/owlplug.ico -BinstalldirChooser=true -BmenuHint=true -BshortcutHint=true -Bvendor=OwlPlug
+-srcdir ../owlplug-client/target/ -srcfiles owlplug-client-%owlplug-version%.jar;LICENSE -outdir ./output -outfile owlplug-app ^
+-name OwlPlug -BappVersion=%owlplug-version% -Bicon=resources/owlplug.ico -BinstalldirChooser=true -BmenuHint=true ^
+-BshortcutHint=true -Bvendor=OwlPlug -BlicenseFile=LICENSE
 
 echo ** OwlPlug MSI Install package generated**
