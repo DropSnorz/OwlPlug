99d8a609b9c30be1f7cc079c64d4aa55b735ef66
Arthur POIRET <dropsnorz@gmail.com>
2019-04-27 10:56:05 +0200

    fix issue #6 plugin bundles can't be removed from UI

--- owlplug-client/src/main/java/com/owlplug/core/tasks/PluginRemoveTask.java
+++ owlplug-client/src/main/java/com/owlplug/core/tasks/PluginRemoveTask.java
@@ -3,8 +3,14 @@ package com.owlplug.core.tasks;
 import com.owlplug.core.dao.PluginDAO;
 import com.owlplug.core.model.Plugin;
 import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PluginRemoveTask extends AbstractTask {
+
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
 
   protected Plugin plugin;
   protected PluginDAO pluginDAO;
@@ -24,9 +30,21 @@ public class PluginRemoveTask extends AbstractTask {
     this.updateMessage("Deleting plugin " + plugin.getName() + " ...");
 
     File pluginFile = new File(plugin.getPath());
-    if (pluginFile.delete()) {
+    boolean fileDeleteSuccess = false;
+    if(pluginFile.exists() && pluginFile.isDirectory()) {
+      try {
+        FileUtils.deleteDirectory(pluginFile);
+        fileDeleteSuccess = true;
+      } catch (IOException e) {
+        log.error("Plugin File can't be removed: " + pluginFile.getPath(), e);
+      }
+    } else {
+      fileDeleteSuccess = pluginFile.delete();
+    }
+    
+    
+    if (fileDeleteSuccess) {
       pluginDAO.delete(plugin);
-
       this.updateProgress(1, 1);
       this.updateMessage("Plugin successfully deleted");
 
