7fc364056b4a71b862f1caff1accac3eb1b8ecbc
Arthur POIRET <dropsnorz@gmail.com>
2019-07-16 20:45:20 +0200

    fix #12 MSI installer keeps outdated jars after an update

--- installer/package-msi.cmd
+++ installer/package-msi.cmd
@@ -7,10 +7,14 @@ echo ** Exporting LICENSE File **
 
 copy "..\LICENSE" "..\owlplug-client\target\LICENSE"
 
+echo ** Copying owlplug-client-%owlplug-version%.jar to owlplug.jar **
+
+copy "..\owlplug-client\target\owlplug-client-%owlplug-version%.jar" "..\owlplug-client\target\owlplug.jar"
+
 echo ** Generating OwlPlug MSI Install package **
 
 javapackager -deploy -native msi -appclass org.springframework.boot.loader.JarLauncher ^
--srcdir ../owlplug-client/target/ -srcfiles owlplug-client-%owlplug-version%.jar;LICENSE -outdir ./output -outfile owlplug-app ^
+-srcdir ../owlplug-client/target/ -srcfiles owlplug.jar;LICENSE -outdir ./output -outfile owlplug-app ^
 -name OwlPlug -BappVersion=%owlplug-version% -Bicon=resources/owlplug.ico -BinstalldirChooser=true -BmenuHint=true ^
 -BshortcutHint=true -Bvendor=OwlPlug -BlicenseFile=LICENSE
 
