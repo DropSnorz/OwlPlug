4f6e5d0c178cfa11efb7394d7f8508c1f0b3b90c
Arthur Poiret <contact@arthurpoiret.fr>
2021-04-28 23:56:58 +0200

    chore: update license code header

--- owlplug-client/src/main/java/com/owlplug/core/ui/RecoveredPluginView.java
+++ owlplug-client/src/main/java/com/owlplug/core/ui/RecoveredPluginView.java
@@ -2,14 +2,13 @@ package com.owlplug.core.ui;
 
 import com.jfoenix.controls.JFXToggleButton;
 /* OwlPlug
- * Copyright (C) 2019 Arthur <dropsnorz@gmail.com>
+ * Copyright (C) 2021 Arthur <dropsnorz@gmail.com>
  *
  * This file is part of OwlPlug.
  *
  * OwlPlug is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License version 3 
+ * as published by the Free Software Foundation.
  *
  * OwlPlug is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
