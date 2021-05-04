f85a0d62463424a98bca01bfaa017fd32fe6f0cd
Arthur Poiret <contact@arthurpoiret.fr>
2021-05-05 01:14:12 +0200

    build: bump owlplug-host version to 0.2.0

--- owlplug-host/src/main/juce/JuceLibraryCode/JuceHeader.h
+++ owlplug-host/src/main/juce/JuceLibraryCode/JuceHeader.h
@@ -46,7 +46,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "owlplug-host";
     const char* const  companyName    = "OwlPlug";
-    const char* const  versionString  = "0.1.2";
-    const int          versionNumber  = 0x102;
+    const char* const  versionString  = "0.2.0";
+    const int          versionNumber  = 0x200;
 }
 #endif
