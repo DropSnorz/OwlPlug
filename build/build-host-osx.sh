d03fbb26fc0cf59a7ab090bb5c33ee1e67cf5eb4
Arthur Poiret <contact@arthurpoiret.fr>
2021-07-12 23:07:11 +0200

    ci: update projucer source

--- build/build-host-osx.sh
+++ build/build-host-osx.sh
@@ -5,7 +5,7 @@ echo $JAVA_HOME
 ls $JAVA_HOME/include/darwin
 
 # Resave jucer files
-"$ROOT/build/bin/Projucer.app/Contents/MacOS/Projucer" --resave "$ROOT/owlplug-host/src/main/juce/OwlPlugHost.jucer"
+"$ROOT/build/bin/JUCE/Projucer.app/Contents/MacOS/Projucer" --resave "$ROOT/owlplug-host/src/main/juce/OwlPlugHost.jucer"
 
 cd "$ROOT/owlplug-host/src/main/juce/Builds/MacOSX"
 xcodebuild -configuration Release || exit 1
