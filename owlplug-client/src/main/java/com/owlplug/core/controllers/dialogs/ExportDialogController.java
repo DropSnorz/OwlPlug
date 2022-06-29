ce25490ed9458640e32893f83be1773e9ce3d74c
Arthur Poiret <contact@arthurpoiret.fr>
2022-06-29 20:19:56 +0200

    feat: save export content as file

--- owlplug-client/src/main/java/com/owlplug/core/controllers/dialogs/ExportDialogController.java
+++ owlplug-client/src/main/java/com/owlplug/core/controllers/dialogs/ExportDialogController.java
@@ -7,15 +7,23 @@ import com.owlplug.core.model.Plugin;
 import com.owlplug.core.model.serializer.PluginCSVSerializer;
 import com.owlplug.core.model.serializer.PluginJsonSerializer;
 import com.owlplug.core.services.PluginService;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ExportDialogController extends AbstractDialogController {
+
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
 
   @Autowired
   private LazyViewRegistry lazyViewRegistry;
@@ -28,6 +36,12 @@ public class ExportDialogController extends AbstractDialogController {
   private TextArea exportTextArea;
   @FXML
   private JFXButton closeButton;
+  @FXML
+  private JFXButton saveAsButton;
+
+  ExportDialogController() {
+    super(450, 500);
+  }
 
   public void initialize() {
     closeButton.setOnAction(e -> {
@@ -36,6 +50,30 @@ public class ExportDialogController extends AbstractDialogController {
 
     exportComboBox.getSelectionModel().selectedItemProperty().addListener((options, oldValue, newValue) -> {
       refreshView();
+    });
+
+    saveAsButton.setOnAction(event -> {
+
+      FileChooser fileChooser = new FileChooser();
+      fileChooser.setTitle("Save");
+
+      String exportType = exportComboBox.getSelectionModel().getSelectedItem();
+      if("CSV".equals(exportType)) {
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV File", ".csv"));
+      } else if ("JSON".equals(exportType)) {
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Json File", ".json"));
+      }
+      fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("All Files", "*.*"));
+
+      File file = fileChooser.showSaveDialog(saveAsButton.getScene().getWindow());
+      if(file != null) {
+        try (FileWriter writer = new FileWriter(file)) {
+          writer.write(exportTextArea.getText());
+          writer.flush();
+        } catch (IOException e) {
+          log.error("Error during writing on file {}", file.getAbsolutePath(), e);
+        }
+      }
     });
   }
 
