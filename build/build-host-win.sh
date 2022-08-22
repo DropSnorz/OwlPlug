3ecffbe4fffb512503a3c464d252c334cca2ffda
Arthur Poiret <dropsnorz@gmail.com>
2022-08-22 20:30:42 +0200

    fix: windows build path

--- build/build-host-win.sh
+++ build/build-host-win.sh
@@ -8,6 +8,6 @@ VS_WHERE="C:/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe"
 MSBUILD_EXE=$("$VS_WHERE" -latest -requires Microsoft.Component.MSBuild -find "MSBuild\**\Bin\MSBuild.exe")
 echo $MSBUILD_EXE
 
-cd "$ROOT/owlplug-host/src/main/juce/Builds/VisualStudio2017"
-"$MSBUILD_EXE" "owlplug-host.sln" "//p:VisualStudioVersion=17.0" "//m" "//t:Build" "//p:Configuration=Release" "//p:PlatformTarget=x64" "//p:PreferredToolArchitecture=x64"
+cd "$ROOT/owlplug-host/src/main/juce/Builds/VisualStudio2019"
+"$MSBUILD_EXE" "owlplug-host.sln" "//p:VisualStudioVersion=19.0" "//m" "//t:Build" "//p:Configuration=Release" "//p:PlatformTarget=x64" "//p:PreferredToolArchitecture=x64"
 
