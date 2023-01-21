11e3850a572349f8a5a493010c1fec505ca3850c
Arthur Poiret <contact@arthurpoiret.fr>
2023-01-21 11:19:21 +0100

    feat: add multi-archive support

--- owlplug-client/src/main/java/com/owlplug/core/utils/FileUtils.java
+++ owlplug-client/src/main/java/com/owlplug/core/utils/FileUtils.java
@@ -119,54 +119,6 @@ public class FileUtils {
     }
   }
 
-  public static void unzip(String source, String dest) throws IOException {
-
-    // Open the file
-    try (ZipFile file = new ZipFile(source)) {
-      FileSystem fileSystem = FileSystems.getDefault();
-      // Get file entries
-      Enumeration<? extends ZipEntry> entries = file.entries();
-
-      // We will unzip files in this folder
-      String uncompressedDirectory = dest;
-      Files.createDirectory(fileSystem.getPath(uncompressedDirectory));
-
-      // Iterate over entries
-      while (entries.hasMoreElements()) {
-        ZipEntry entry = entries.nextElement();
-        // If directory then create a new directory in uncompressed folder
-        if (entry.isDirectory()) {
-          log.debug("Creating Directory:" + uncompressedDirectory + File.separator + entry.getName());
-          Files.createDirectories(fileSystem.getPath(uncompressedDirectory + File.separator + entry.getName()));
-
-          // Else create the file
-        } else {
-          InputStream is = file.getInputStream(entry);
-          BufferedInputStream bis = new BufferedInputStream(is);
-          String uncompressedFileName = uncompressedDirectory + File.separator + entry.getName();
-
-          new File(uncompressedFileName).getParentFile().mkdirs();
-
-          Path uncompressedFilePath = fileSystem.getPath(uncompressedFileName);
-          Files.createFile(uncompressedFilePath);
-
-          FileOutputStream fileOutput = new FileOutputStream(uncompressedFileName);
-
-          byte[] buffer = new byte[2048];
-          int read = 0;
-          while ((read = bis.read(buffer)) > 0) {
-            fileOutput.write(buffer, 0, read);
-          }
-
-          fileOutput.close();
-          log.debug("Written :" + entry.getName());
-        }
-      }
-    } catch (IOException e) {
-      throw e;
-    }
-  }
-
   public static void copyDirectory(File source, File target) throws IOException {
     org.apache.commons.io.FileUtils.copyDirectory(source, target);
   }
