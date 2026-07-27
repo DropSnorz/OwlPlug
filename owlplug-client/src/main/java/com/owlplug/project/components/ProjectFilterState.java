3ddf52372862f2112fc6c6be3265fd66d053f334
Arthur Poiret <contact@arthurpoiret.fr>
2026-07-27 21:48:40 +0200

    feat: add project tree view filtering

---
+++ owlplug-client/src/main/java/com/owlplug/project/components/ProjectFilterState.java
@@ -0,0 +1,59 @@
+/* OwlPlug
+ * Copyright (C) 2021 Arthur <dropsnorz@gmail.com>
+ *
+ * This file is part of OwlPlug.
+ *
+ * OwlPlug is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * OwlPlug is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OwlPlug.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.owlplug.project.components;
+
+import com.owlplug.project.model.DawApplication;
+import com.owlplug.project.model.DawProject;
+import java.util.function.Predicate;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableSet;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProjectFilterState {
+
+  private final ObservableSet<DawApplication> selectedApplications = FXCollections.observableSet();
+  private final ObjectProperty<Predicate<DawProject>> predicate = new SimpleObjectProperty<>();
+
+  public ProjectFilterState() {
+    predicate.bind(Bindings.createObjectBinding(this::buildPredicate, selectedApplications));
+  }
+
+  private Predicate<DawProject> buildPredicate() {
+    if (selectedApplications.isEmpty()) {
+      return null;
+    }
+    return project -> selectedApplications.contains(project.getApplication());
+  }
+
+  public ObservableSet<DawApplication> getSelectedApplications() {
+    return selectedApplications;
+  }
+
+  public ObjectProperty<Predicate<DawProject>> predicateProperty() {
+    return predicate;
+  }
+
+  public void clearAll() {
+    selectedApplications.clear();
+  }
+}
